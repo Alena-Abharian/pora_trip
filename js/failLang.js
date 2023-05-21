b74b2d272128cb3be607cc8e5f05e2631d756f47
Alena Abharian <alenar2531@gmail.com>
2023-05-20 22:09:48 -0700

    add slider and chenge language

--- js/failLang.js
+++ js/failLang.js
@@ -1,14 +1,14 @@
 export const landingText = {
     "main_title": {
-        "ru": "Пора вырваться из душного мегаполиса",
+        "ru": "Треба вирватися з задушливого мегаполісу",
         "en": "It's time to escape from the stuffy metropolis",
     },
     "main_text": {
-        "ru": "Отдых по всему миру, который вы заслужили",
+        "ru": "Відпочинок по всьому світу, який ви заслужили",
         "en": "The worldwide vacation you deserve",
     },
     "our_tours": {
-        "ru": "Наши туры",
+        "ru": "Наші тури",
         "en": "Our tours",
     },
     "gallery": {
@@ -16,100 +16,102 @@ export const landingText = {
         "en": "Gallery",
     },
     "travel_history": {
-        "ru": "Истории путешествий ",
+        "ru": "Історії подорожей",
         "en": "Travel history",
     },
     "contacts": {
-        "ru": "Контакты",
+        "ru": "Контакти",
         "en": "Contacts",
     },
     "main_btn": {
-        "ru": "Подробнее",
+        "ru": "Детальніше",
         "en": "More details",
     },
     "video_title_1": {
-        "ru": "20км от Геленджика",
+        "ru": "20км від Геленджика",
         "en": "20 km from Gelendzhik",
     },
     "video_text_1": {
-        "ru": "Чудесное скопление скал",
+        "ru": "Чудове скупчення скель",
         "en": "Wonderful collection of rocks",
     },
     "video_title_2": {
-        "ru": "Северное сияние",
+        "ru": "Північне сяйво",
         "en": "Northern lights",
     },
     "video_text_2": {
-        "ru": "2 недели в Финляндии",
+        "ru": "2 тижні у Фінляндії",
         "en": "2 weeks in Finland",
     },
     "about_1": {
-        "ru": "С любовью и заботой организуем для вас долгожданный отдых в самых\n" +
-            "интересных уголках планеты. В далеком 2009 мы с женой впервые\n" +
-            "организовали тур для наших друзей.",
+        "ru": "З любов'ю та турботою організуємо для вас довгоочікуваний відпочинок у найцікавіших куточках планети. " +
+            "У далекому 2009 році ми з дружиною вперше організували тур для наших друзів.",
         "en": "With love and care we will organize for you a long-awaited vacation in\n" +
             "          the most interesting corners of the planet. Back in 2008, my wife and\n" +
             "          I for the first time organized a tour for our friends.",
     },
     "about_2": {
-        "ru": "С того дня это стало делом всей жизни.\n" +
-            "Сейчас вместе с нами в команде уже 27, а количество путешественников,\n" +
-            "которым мы помогли уже перевалило за четыре тысячи.",
+        "ru": "З того дня це стало справою всього життя. Зараз разом із нами у команді вже 27, " +
+            "а кількість мандрівників, яким ми допомогли, вже перевалило за чотири тисячі.",
         "en": " From that day on, it became a matter of life. Now with us in the team\n" +
             "          there are already 27, and the number of travelers, whom we helped, has\n" +
             "          already exceeded four thousand.",
     },
     "btn_play": {
-        "ru": "Наша история за 90 секунд",
+        "ru": "Наша історія за 90 секунд",
         "en": "Our story in 90 seconds",
     },
    "text_1": {
-        "ru": "Путешественников уже вместе с нами",
+        "ru": "Мандрівників уже разом із нами",
         "en": "Travelers are already with us",
     },
     "text_2": {
-        "ru": "Наших клиентов приходят повторно",
+        "ru": "Наших клієнтів приходять повторно",
         "en": "Our clients come back",
     },
     "text_3": {
-        "ru": "Необычных локаций для путешествий",
+        "ru": "Незвичайні локації для подорожей",
         "en": "Unusual places to travel",
+    },
+    "text_4": {
+        "ru": "Років даруємо вам нові емоції",
+        "en": "Years we give you new emotions",
     },
     "gallery_title": {
         "ru": "Галерея",
         "en": "Gallery",
     },
     "gallery_text": {
-        "ru": "В каждоме путешествии с вами будет фотограф, который поможет всё запечатлить",
+        "ru": "У кожній подорожі з вами буде фотограф, який допоможе все сфотографувати",
         "en": "On every journey a photographer will be with you, he will help take it\n" +
             "          all off",
     },
     "insta": {
-        "ru": "Больше фотографий смотрите в нашем",
+        "ru": "Більше фотографій дивіться у нашому",
         "en": "See more photos on our",
     },
     "photo_title": {
-        "ru": "Озеро на Микли",
+        "ru": "Озеро на Міклі",
         "en": "Lake on Mikli",
     },
     "photo_text": {
-        "ru": "Сердце Карелии",
+        "ru": "Серце Карелії",
         "en": "Heart of Karelia",
     },
     "mikli_title": {
-        "ru": "Большой выезд на хутор Микли",
+        "ru": "Великий виїзд на хутір Міклі",
         "en": "Great trip to Mikli Farm",
     },
     "mikli_text": {
-        "ru": "Оставьте заявку и мы перезвоним вам в течение трёх минут",
+        "ru": "Залишіть заявку і ми передзвонимо вам протягом трьох хвилин",
         "en": "Leave a request and we will call you back within three minutes",
     },
     "mikli_btn": {
-        "ru": "Оставить заявку",
+        "ru": "Залишити заявку",
         "en": "Submit",
     },
     "call-pop-up": {
-        "ru": "Пожалуйста, подождите. Мы свяжемся с вами в течение трех минут.",
+        "ru": "Будь ласка зачекайте. Ми зв'яжемося з вами протягом трьох хвилин.",
         "en": "Please wait. We will contact you in three minutes.",
     },
 };
---
+++ js/slider.js
@@ -0,0 +1,28 @@
+const sliderImg = document.querySelector('[data-img]');
+const pagination = document.querySelector('.photo_count');
+
+const imgArr = ['imgMikli.jpg', 'Mikli2.jpg', 'Mikli3.jpg', 'Mikli4.jpg'];
+const numberArr = ['1/4','2/4', '3/4', '4/4'];
+
+let i = 0; //current img index
+
+//btn-1 prev
+document.querySelector('.photo_btn-1').addEventListener('click', function () {
+    if(i <= 0) i = imgArr.length;
+    i--;
+    return setImg();
+})
+
+//btn-2 next
+document.querySelector('.photo_btn-2').addEventListener('click', function () {
+    if(i >= imgArr.length-1) i = -1;
+    i++;
+    return setImg();
+})
+
+
+
+function setImg() {
+        sliderImg.setAttribute('src', './image/' + imgArr[i])
+        pagination.innerText = numberArr[i]
+}
